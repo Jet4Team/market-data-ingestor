714e86e8b7e8170cd5d73b4813b2c9f290e82196
Cuber <aefimov.cube@gmail.com>
2019-08-31 23:29:17 -0700

    Trades event stream model

--- src/model.rs
+++ src/model.rs
@@ -1,7 +1,46 @@
 #[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct TradesEvent {
+    #[serde(rename = "e")]
+    pub event_type: String,
+
+    #[serde(rename = "E")]
+    pub event_time: u64,
+
+    #[serde(rename = "s")]
+    pub symbol: String,
+
+    #[serde(rename = "t")]
+    pub trade_id: u64,
+
+    #[serde(rename = "p")]
+    pub price: String,
+
+    #[serde(rename = "q")]
+    pub qty: String,
+
+    #[serde(rename = "b")]
+    pub buyer_trade_id: u64,
+
+    #[serde(rename = "a")]
+    pub seller_trade_id: u64,
+
+    #[serde(rename = "T")]
+    pub trade_order_time: u64,
+
+    #[serde(rename = "m")]
+    pub is_buyer_maker: bool,
+
+    #[serde(skip, rename = "M")]
+    pub m_ignore: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Bids {
-    #[serde(with = "string_or_float")] pub price: f64,
-    #[serde(with = "string_or_float")] pub qty: f64,
+    #[serde(with = "string_or_float")]
+    pub price: f64,
+    #[serde(with = "string_or_float")]
+    pub qty: f64,
 
     #[serde(skip)]
     ignore: Vec<String>,
@@ -9,8 +48,10 @@ pub struct Bids {
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Asks {
-    #[serde(with = "string_or_float")] pub price: f64,
-    #[serde(with = "string_or_float")] pub qty: f64,
+    #[serde(with = "string_or_float")]
+    pub price: f64,
+    #[serde(with = "string_or_float")]
+    pub qty: f64,
 
     #[serde(skip)]
     ignore: Vec<String>,
@@ -19,19 +60,26 @@ pub struct Asks {
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct DepthOrderBookEvent {
-    #[serde(rename = "e")] pub event_type: String,
+    #[serde(rename = "e")]
+    pub event_type: String,
 
-    #[serde(rename = "E")] pub event_time: u64,
+    #[serde(rename = "E")]
+    pub event_time: u64,
 
-    #[serde(rename = "s")] pub symbol: String,
+    #[serde(rename = "s")]
+    pub symbol: String,
 
-    #[serde(rename = "U")] pub first_update_id: u64,
+    #[serde(rename = "U")]
+    pub first_update_id: u64,
 
-    #[serde(rename = "u")] pub final_update_id: u64,
+    #[serde(rename = "u")]
+    pub final_update_id: u64,
 
-    #[serde(rename = "b")] pub bids: Vec<Bids>,
+    #[serde(rename = "b")]
+    pub bids: Vec<Bids>,
 
-    #[serde(rename = "a")] pub asks: Vec<Asks>,
+    #[serde(rename = "a")]
+    pub asks: Vec<Asks>,
 }
 
 mod string_or_float {
@@ -40,14 +88,16 @@ mod string_or_float {
     use serde::{de, Deserialize, Deserializer, Serializer};
 
     pub fn serialize<T, S>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
-        where T: fmt::Display,
-              S: Serializer
+    where
+        T: fmt::Display,
+        S: Serializer,
     {
         serializer.collect_str(value)
     }
 
     pub fn deserialize<'de, D>(deserializer: D) -> Result<f64, D::Error>
-        where D: Deserializer<'de>
+    where
+        D: Deserializer<'de>,
     {
         #[derive(Deserialize)]
         #[serde(untagged)]
